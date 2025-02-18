3841155951ffc1764ec38664aa00321e17f0379f
sundarapallisivasankar <166827964+sundarapallisivasankar@users.noreply.github.com>
2025-02-18 12:18:11 +0530

    Create unixfork.c

---
+++ unixfork.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(){
+	int pid,pid1,pid2;
+	pid=fork();
+	if(pid==-1){
+		printf("Error in process creation\n");
+		exit(0);
+	}
+	if(pid!=0){
+		pid1=getpid();
+		printf("\n The parent process ID is %d\n",pid1);
+	}else{
+		pid2=getpid();
+		printf("\n The child process ID is%d\n",pid2);
+	}
+	return 0;
+}
